ad2bac169069518b78583d8853996832917634ae
lennyz0689 <lennyz0689@gmail.com>
2023-06-05 18:12:19 -0500

    08 React Estados complete exercises 1.2

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -7,28 +7,54 @@ import Species from "../Species/Species";
 import { useState } from "react";
 
 
-export default function Zoo() {
+const Zoo = () => {
   /* Escribe acá tu código */
 
   const [zoo, setZoo] = React.useState({
     zooName: '',
     animals: [],
     species: [],
-    allAnimals: [] 
+    allAnimals: []
   })
 
-  const handleInputChange = (evento) => {
+  const handleInputChange = (event) => {
     setZoo({
       ...zoo,
-      zooName: evento.target.value
+      zooName: event.target.value
     })
   };
+
+  React.useEffect(() => {
+    fetch('http://localhost:3001/zoo')
+      .then((res) => res.json())
+      .then((data) =>
+        setZoo({
+          ...zoo,
+          animals: data.animals,
+          species: data.species,
+          allAnimals: data.animals,
+        })
+      )
+      .catch((error) => console.log(error));
+  }, [])
+
+  const handleSpecies = (event) => {
+
+  }
+
+  const handleAllSpecies = () => {
+
+  }
 
   return (
     <div>
       <label>Zoo Name:</label>
       <input type="text" value={zoo.zooName} onChange={handleInputChange} />
       <h1>{zoo.zooName}</h1>
+      <Species species={zoo.species} handleSpecies={handleSpecies} handleAllSpecies={handleAllSpecies}/>
+      <Animals animals={zoo.animals}/>
     </div>
   );
 }
+
+export default Zoo
