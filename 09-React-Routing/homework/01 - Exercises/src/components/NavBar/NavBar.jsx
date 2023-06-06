755c11b8b5a290555230b1e32afcec44b0bff010
lennyz0689 <lennyz0689@gmail.com>
2023-06-06 13:07:35 -0500

    09 React routing complete 1.3

--- 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
+++ 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import logoHenry from "../../assets/logo-henry.png";
 import styleNav from "./NavBar.module.css";
+import { NavLink } from "react-router-dom"
+
+const className = ({isActive}) => {
+if ({isActive} === true) {
+  return styleNav.active
+}else{
+  return styleNav.disable
+}
+} 
+
 
 export default function NavBar() {
   return (
     <div className={styleNav.container}>
       <ul className={styleNav.menu}>
+        <NavLink to="/">
         <li>
           <img src={logoHenry} alt="logo-henry" />
         </li>
+        </NavLink>
         <li>
           <h1>Central de Cruceros</h1>
         </li>
         <div className={styleNav.options}>
+          <NavLink to="/shipping">
           <li>
             <span>Navieras</span>
           </li>
+          </NavLink>
+          <NavLink to="/discounts" className={className}>
           <li>
             <span>Promociones</span>
           </li>
+          </NavLink>
         </div>
       </ul>
     </div>
