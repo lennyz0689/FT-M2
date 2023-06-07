3a2be2494bdef91ca3a6d146f4c32546817835a0
lennyz0689 <lennyz0689@gmail.com>
2023-06-06 19:10:28 -0500

    10 React forms complete 1.1

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -5,5 +5,17 @@ import './Contact.modules.css'
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export default function Contact () {
-  return <div>Crear Formulario</div>
+  return (
+  <div>
+    <form>
+      <label>Nombre:</label>
+      <input name='name' placeholder='Escribe tu nombre...' type='text' />
+      <label>Correo Electrónico:</label>
+      <input name='email' placeholder='Escribe tu email...' type="text" />
+      <label>Mensaje:</label>
+      <textarea name="message" placeholder='Escribe tu mensaje...' type='text'></textarea>
+      <button type='submit'>Enviar</button>
+    </form>
+  </div>
+  )
 }
